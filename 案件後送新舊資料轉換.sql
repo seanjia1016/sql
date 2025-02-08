d82608fff793fc2bbf6fd3999b47d8c807ce6217
seanjia1016 <digiwin.sean@gmail.com>
2025-02-08 19:04:56 +0800

    no message

--- 案件後送新舊資料轉換.sql
+++ 案件後送新舊資料轉換.sql
@@ -55,7 +55,6 @@ FROM OLD_FUNC_TO_NEW;
 SELECT *
 FROM #funcIDMapping;
 
-
 /*--------------------執行新舊主表轉移結束--------------------*/
 
 /*--------------------執行新舊案件分類轉移開始--------------------*/
@@ -103,7 +102,99 @@ SELECT FUNC_TYPE_NAME,
        MODIFY_USER,
        IS_DEFAULT
 FROM OLD_TYPE_TO_NEW
+/*--------------------執行新舊案件分類轉移結束--------------------*/
 
+/*--------------------執行新舊案件自定義欄位轉移開始--------------------*/
+CREATE TABLE #funcFieldIDMapping
+(
+    OLD_FUNC_FIELD_ID INT,
+    NEW_FUNC_FIELD_ID BIGINT
+);
+;
+WITH OLD_FUNC_FIELD_TO_NEW AS (SELECT tTCT.DBID                        AS FUNC_FIELD_ID,
+                                      tTCT.Title                       AS FUNC_FIELD_NAME,
+                                      tTCT.description                 AS FUNC_FIELD_COMMENTS,
+                                      tTCT.TitleType                   AS FUNC_FIELD_TYPE,
+                                      IIF(tTCT.IsRequire = 'N', 0, 1)  AS REQUIRED,
+                                      IIF(tTCT.DeleteFlag = 'N', 1, 2) AS ACTIVE_STATUS,
+                                      CAST(GETDATE() AS DATETIME2)     AS MODIFY_TIME,
+                                      0                                AS MODIFY_USER, --'SYSTEM'
+                                      tTT.TenantID                     AS TENANT_ID,
+                                      CASE tTCT.TitleType
+                                          WHEN 'input' THEN 'basic-text'
+                                          WHEN 'inputforEnglish' THEN 'basic-text-english'
+                                          WHEN 'inputforNumber' THEN 'basic-text-number'
+                                          WHEN 'inputforNumberAndEnglish' THEN 'basic-text-number-english'
+                                          WHEN 'inputforNumberAndSign' THEN 'basic-text-number-sign'
+                                          WHEN 'inputforSignAndEnglish' THEN 'basic-text-english-sign'
+                                          WHEN 'select' THEN 'basic-select'
+                                          WHEN 'selectlist' THEN 'multiple-select'
+                                          WHEN 'cusData' THEN 'text-with-switch-btn'
+                                          ELSE 'Unknown funcFieldType'
+                                          END                          AS SEARCH_COL_TYPE
+                               FROM tblTicketColTitle tTCT
+                                        JOIN tblTicketTemplate tTT ON (tTCT.TicketTemplateID = tTT.DBID)
+                               WHERE tTCT.Type = 'castom')
+INSERT
+INTO tblCfg_FuncField (FUNC_FIELD_NAME,
+                       FUNC_FIELD_COMMENTS,
+                       FUNC_FIELD_TYPE,
+                       REQUIRED,
+                       ACTIVE_STATUS,
+                       MODIFY_TIME,
+                       MODIFY_USER, --'SYSTEM'
+                       TENANT_ID,
+                       SEARCH_COL_TYPE)
+OUTPUT inserted.FUNC_FIELD_ID,
+       OLD_FUNC_FIELD_TO_NEW.FUNC_FIELD_ID
+    INTO #funcFieldIDMapping (NEW_FUNC_FIELD_ID, OLD_FUNC_FIELD_ID)
+SELECT FUNC_FIELD_NAME,
+       FUNC_FIELD_COMMENTS,
+       FUNC_FIELD_TYPE,
+       REQUIRED,
+       ACTIVE_STATUS,
+       MODIFY_TIME,
+       MODIFY_USER, --'SYSTEM'
+       TENANT_ID,
+       SEARCH_COL_TYPE
+FROM OLD_FUNC_FIELD_TO_NEW
+
+/*--------------------執行新舊自定義欄位轉移結束--------------------*/
+
+/*--------------------執行新舊案件主表和自定義欄位Mapping表轉移開始--------------------*/
+DECLARE @NEW_FUNC_FIELD_MAPPING_ID BIGINT
+;
+WITH OLD_FUNC_FIELD_MAPPING_TO_NEW AS (SELECT t2.NEW_FUNC_ID                                                                          AS FUNC_ID,
+                                              t3.NEW_FUNC_FIELD_ID                                                                    AS FUNC_FIELD_ID,
+                                              IIF(tTCT.ColType = 'Open', 1, 0)                                                        AS TICKET_DISPLAY,
+                                              IIF(tTCT.ColType = 'Open', 1, 0)                                                        AS TICKET_FORM_HEADER_DISPLAY,
+                                              1                                                                                       AS TICKET_REPLY_DISPLAY,
+                                              ROW_NUMBER() over (PARTITION BY t2.NEW_FUNC_ID,t3.NEW_FUNC_FIELD_ID ORDER BY tTCT.Sort) AS FUNC_FIELD_SORT,
+                                               tTCT.DeleteFlag                                                                         AS DELETE_FLAG
+                                       FROM (SELECT DISTINCT tTT.DBID AS OLD_FUNC_ID, tTCT.DBID AS OLD_FUNC_FIELD_ID
+                                             FROM tblTicketColTitle tTCT
+                                                      JOIN dbo.tblTicketTemplate tTT ON (tTCT.TicketTemplateID = tTT.DBID)) AS t1
+                                                JOIN tblTicketColTitle tTCT ON (t1.OLD_FUNC_FIELD_ID = tTCT.DBID)
+                                                JOIN #FuncIDMapping t2 ON (t1.OLD_FUNC_ID = t2.OLD_FUNC_ID)
+                                                JOIN #funcFieldIDMapping t3 ON (t1.OLD_FUNC_FIELD_ID = t3.OLD_FUNC_FIELD_ID))
+INSERT
+INTO tblCfg_FuncFieldMapping(
+    FUNC_ID,
+    FUNC_FIELD_ID,
+    TICKET_DISPLAY,
+    TICKET_FORM_HEADER_DISPLAY,
+    TICKET_REPLY_DISPLAY,
+    FUNC_FIELD_SORT,
+    DELETE_FLAG)
+OUTPUT @NEW_FUNC_FIELD_MAPPING_ID = inserted.DBID
+SELECT FUNC_ID,
+       FUNC_FIELD_ID,
+       TICKET_DISPLAY,
+       TICKET_FORM_HEADER_DISPLAY,
+       TICKET_REPLY_DISPLAY,
+       FUNC_FIELD_SORT,
+       DELETE_FLAG
+FROM OLD_FUNC_FIELD_MAPPING_TO_NEW
 /*--------------------執行新舊工單轉移開始--------------------*/
 CREATE TABLE #ticketIDMapping
 (
@@ -118,7 +209,7 @@ WITH OLD_TICKET_TO_NEW AS (SELECT tT.DBID                                       
                                   CASE tT.TicketStatus
                                       WHEN 'ACCEPT' THEN 2
                                       WHEN 'TRANSFER' THEN 2
-                                      WHEN 'REOPEN' THEN 7
+                                      WHEN 'REQOPEN' THEN 7
                                       WHEN 'CLOSED' THEN 5
                                       WHEN 'DELETE' THEN 6
                                       END                                               AS FUNC_STATUS_ID,
@@ -128,7 +219,7 @@ WITH OLD_TICKET_TO_NEW AS (SELECT tT.DBID                                       
                                   tT.typeID                                             AS CONTACT_TYPE_ID,
                                   tIII.IxnID                                            AS IXN_ID,
                                   tT.Subject                                            AS TICKET_SUBJECT,
-                                  tTSI.Content                                          AS Content,
+                                  tTSI.Content                                          AS TICKET_CONTENT,
                                   tT.PriorityID                                         AS TICKET_PRIORITY,
                                   tT.Memo                                               AS TICKET_MEMO,
                                   tTSI2.TicketContentText                               AS TICKET_REPLY_CONTENT,
@@ -198,7 +289,7 @@ INTO tblRpt_Ticket(FUNC_ID,
                    CONTACT_TYPE_ID,
                    IXN_ID,
                    TICKET_SUBJECT,
-                   Content,
+                   TICKET_CONTENT,
                    TICKET_PRIORITY,
                    TICKET_MEMO,
                    TICKET_REPLY_CONTENT,
@@ -215,23 +306,70 @@ INTO tblRpt_Ticket(FUNC_ID,
                    ACTIVE_STATUS)
     OUTPUT inserted.TICKET_ID,
     OLD_TICKET_TO_NEW.TICKET_ID -- 記錄新舊PK關係
-INSERT INTO #ticketIDMapping(NEW_TICKET_ID, OLD_TICKET_ID)
+INTO #ticketIDMapping (NEW_TICKET_ID, OLD_TICKET_ID)
+SELECT FUNC_ID,
+       FUNC_STATUS_ID,
+       FUNC_TYPE_ID,
+       TENANT_ID,
+       CONTACT_ID,
+       CONTACT_TYPE_ID,
+       IXN_ID,
+       TICKET_SUBJECT,
+       TICKET_CONTENT,
+       TICKET_PRIORITY,
+       TICKET_MEMO,
+       TICKET_REPLY_CONTENT,
+       CREATE_TIME,
+       CREATE_USER,
+       HANDLING_PERSON,
+       HANDLING_PILOT,
+       MODIFY_TIME,
+       MODIFY_USER,
+       EXPIRED_TIME,
+       WATCHERS_ID,
+       IS_DRAFT,
+       EMAIL_NOTICE_FLAG,
+       ACTIVE_STATUS
+FROM OLD_TICKET_TO_NEW
 
 /*--------------------執行新舊工單轉移結束--------------------*/
 
+/*--------------------執行新舊工單自定義欄位轉移開始--------------------*/
+CREATE TABLE #ticketFieldIDMapping
+(
+    OLD_TICKET_FIELD_ID INT,
+    NEW_TICKET_FIELD_ID bigint
+)
+-- SELECT tTSI.DBID AS DBID tIM.NEW_TICKET_ID AS TICKET_ID, tTSI
+--
+-- FROM tblTicketSubItem tTSI
+--          JOIN #ticketIDMapping tIM ON tTSI.TicketID = tIM.OLD_TICKET_ID
 
 
-
-
-
-
-
-
-SELECT DISTINCT tblTicket.TicketStatus
-FROM tblTicket
-SELECT *
-FROM tblCfg_FuncMain
-SELECT *
-FROM tblRpt_Ticket
-SELECT *
-FROM tblCfg_FuncType
+-- SELECT DISTINCT tblTicket.TicketStatus
+-- FROM tblTicket
+-- SELECT *
+-- FROM tblCfg_FuncMain
+-- SELECT *
+-- FROM tblRpt_Ticket
+-- SELECT *
+-- FROM tblCfg_FuncType
+-- SELECT *
+-- FROM tblTicketSubItem
+-- SELECT *
+-- FROM tblRpt_TicketField
+-- SELECT *
+-- FROM tblRpt_TicketColData
+-- SELECT *
+-- FROM tblTicketColTitle
+-- SELECT DISTINCT *
+-- FROM tblCfg_FuncField
+-- SELECT *
+-- FROM tblCfg_SolrSearchColumn
+-- SELECT *
+-- FROM tblCfg_FuncFieldMapping
+--
+-- SELECT DISTINCT FUNC_FIELD_TYPE, SEARCH_COL_TYPE
+-- FROM tblCfg_FuncField
+-- WHERE LTRIM(FUNC_FIELD_TYPE) <> 'basic'
+--   AND LTRIM(FUNC_FIELD_TYPE) <> 'system'
