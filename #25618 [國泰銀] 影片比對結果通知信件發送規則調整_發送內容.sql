705b859472d4ec7024802fd9ebc0b8d89cf8c2d5
seanjia1016 <digiwin.sean@gmail.com>
2025-03-04 14:32:18 +0800

    Delete #25618 [國泰銀] 影片比對結果通知信件發送規則調整_發送內容.sql

--- #25618 [國泰銀] 影片比對結果通知信件發送規則調整_發送內容.sql
+++
@@ -1,9 +0,0 @@
---[EXEC]
-UPDATE tblCfg_Application
-SET Value = '影像資料比對項目異常，下方通話筆數無法正確對應影像檔案，請儘速確認。;callid = @callId. 開始通話時間：@startTime. 接聽人員：@agentName. 申請結果:@verifyResult  '
-WHERE TenantID = 3 AND Parameter='lostContent' AND Application='video' AND Setion='videoFileWarningEmail'
-
---[ROLLBACK]
-UPDATE tblCfg_Application
-SET Value = '影像資料比對項目異常，下方通話筆數無法正確對應影像檔案，請儘速確認。;callid = @callId. 開始通話時間：@startTime. 接聽人員：@agentName  '
-WHERE TenantID = 3 AND Parameter='lostContent' AND Application='video' AND Setion='videoFileWarningEmail'
